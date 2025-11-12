ad08f0d13718de1fc0084fd07b37eb1e3f98d69e
alnils <albin.nilsson.se@gmail.com>
2025-11-12 19:04:43 +0100

    creating react vite setup, with bootstrap

---
+++ storeaid/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+      <div className = "bg-primary">
+        <h1>StoreAid Application</h1>
+      </div>
+  )
+}
+
+export default App
