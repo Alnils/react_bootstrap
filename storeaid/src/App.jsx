85b0b1056e7aaf18de43cce05fb44b37f1d9f214
alnils <albin.nilsson.se@gmail.com>
2025-11-12 19:33:41 +0100

    fixed file structure and paths to images

--- storeaid/src/App.jsx
+++ storeaid/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from './components/navbar.jsx'
 
 function App() {
   return (
+    <>
+    <Navbar />
+
       <div className = "bg-primary">
         <h1>StoreAid Application</h1>
       </div>
+    </>
   )
 }
 
