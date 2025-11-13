55a0925b49a624edcc79ec8b07356f5c08e56f75
alnils <albin.nilsson.se@gmail.com>
2025-11-13 21:55:41 +0100

    changes to paths

--- storeaid/src/components/navbar.jsx
+++ storeaid/src/components/navbar.jsx
@@ -6,26 +6,26 @@ export default function navbar() {
     <div className ="navbarcontainer">
         <div className="topcontainer">
             <div className="phoneinformation">
-                <img src="src/imgs/0-header/phone_icon.svg" alt="Phone icon" width="30" height="30"></img>
+                <img src="../imgs/0-header/phone_icon.svg" alt="Phone icon" width="30" height="30"></img>
                 <p>+46 8 123 122 44</p>    
             </div>
             
             <div className="emailinformation">
-                <img src="src/imgs/0-header/mail_icon.svg" alt="Mail icon" width="30" height="30"></img>
+                <img src="../imgs/0-header/mail_icon.svg" alt="Mail icon" width="30" height="30"></img>
                 <p>contact@domain.com</p>
             </div>
 
             <div className="socialmediaicons">
-                <img src="src/imgs/0-header/facebook_icon.svg" alt="Facebook" width="30" height="30"></img>
-                <img src="src/imgs/0-header/twitter_icon.svg" alt="Twitter" width="30" height="30"></img>
-                <img src="src/imgs/0-header/instagram_icon.svg" alt="Instagram" width="30" height="30"></img>
-                <img src="src/imgs/0-header/youtube_icon.svg" alt="YouTube" width="30" height="30"></img>
+                <img src="../imgs/0-header/facebook_icon.svg" alt="Facebook" width="30" height="30"></img>
+                <img src="../imgs/0-header/twitter_icon.svg" alt="Twitter" width="30" height="30"></img>
+                <img src="../imgs/0-header/instagram_icon.svg" alt="Instagram" width="30" height="30"></img>
+                <img src="../imgs/0-header/youtube_icon.svg" alt="YouTube" width="30" height="30"></img>
             </div>
         </div>
 
         <div className="maincontainer">            
             <div className="logo">
-                <img src="src/imgs/0-header/storeaid_logo.svg" alt="Storeaid Logo" width="150" height="50"></img>
+                <img src="../imgs/0-header/storeaid_logo.svg" alt="Storeaid Logo" width="150" height="50"></img>
             </div>
 
             <div className="navigationmenu">
--- storeaid/src/pages/Services.jsx
+++ storeaid/src/pages/Services.jsx
@@ -3,36 +3,36 @@ import React, { Component } from 'react'
 export default class Services extends Component {
     render() {
         return (
-            <section id="services" class="services-section">
+            <section id="services" className="services-section">
 
-                <div class="services-intro">
+                <div className="services-intro">
                     <h3>Our Services</h3>
                     <h2>Specialized Storage for Every Special Item</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
                 </div>
 
-                <div class="services-content">
+                <div className="services-content">
 
-                    <div class="services-cards-left">
-                        <div class="textbox_yellow">
+                    <div className="services-cards-left">
+                        <div className="textbox_yellow">
                             <h2>Diverse Unit Sizes</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                         </div>
-                        <div class="textbox">
+                        <div className="textbox">
                             <h2>Moving Assistance</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                         </div>
                     </div>
 
-                    <div class="greybox_placeholder_services">
+                    <div className="greybox_placeholder_services">
                     </div>
 
-                    <div class="services-cards-right">
-                        <div class="textbox">
+                    <div className="services-cards-right">
+                        <div className="textbox">
                             <h2>Vehicle Storage</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                         </div>
-                        <div class="textbox">
+                        <div className="textbox">
                             <h2>Top-Notch Security</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                         </div>
