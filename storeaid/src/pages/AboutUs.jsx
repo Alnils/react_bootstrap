c9e3f8e9cab92be9efb004956371ebf7b7acb1f6
alnils <albin.nilsson.se@gmail.com>
2025-11-13 17:48:59 +0100

    in home and aboutus pages, changed class to className in code. added ending brackets. new simple styling for better design

--- storeaid/src/pages/AboutUs.jsx
+++ storeaid/src/pages/AboutUs.jsx
@@ -3,50 +3,50 @@ import React, { Component } from 'react'
 export default class AboutUs extends Component {
     render() {
         return (
-            <section id="aboutus" class="aboutus-section">
+            <section id="aboutus" className="aboutus-section">
 
-                <div class="aboutus-content-left">
+                <div className="aboutus-content-left">
 
-                    <div class="greybox_placeholder_aboutus">
+                    <div className="greybox_placeholder_aboutus">
                     </div>
 
-                    <div class="statsbox">
-                        <div class="stat1">
+                    <div className="statsbox">
+                        <div className="stat1">
                             <h2>12+</h2>
                             <p>Years of experience</p>
                         </div>
-                        <div class="stat2">
+                        <div className="stat2">
                             <h2>150K+</h2>
                             <p>Satisfied Clients</p>
                         </div>
-                        <div class="stat3">
+                        <div className="stat3">
                             <h2>35+</h2>
                             <p>Warehouses</p>
                         </div>
                     </div>
                 </div>
 
-                <div class="aboutus-content-right">
-                    <div class="aboutus-text-intro">
+                <div className="aboutus-content-right">
+                    <div className="aboutus-text-intro">
                         <h3>About Us</h3>
                         <h2>Providing a Safe Space for Your Treasured Items</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi, non sodales augue. Phasellus eget elit gravida.</p>
                     </div>
 
-                    <div class="aboutus-textboxes">
-                        <div class="textbox">
+                    <div className="aboutus-textboxes">
+                        <div className="textbox">
                             <h2>Vision</h2>
                             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tium doloremque laudantium.</p>
                         </div>
-                        <div class="textbox_yellow">
+                        <div className="textbox_yellow">
                             <h2>Mission</h2>
                             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tium doloremque laudantium.</p>
                         </div>
                     </div>
 
-                    <div class="aboutus-bottom">
-                        <button class="standardbutton">Discover More</button>
-                        <img src="imgs/2-aboutus/phone_icon.svg" alt="Phone icon" class="phone_icon"></img>
+                    <div className="aboutus-bottom">
+                        <button className="standardbutton">Discover More</button>
+                        <img src="imgs/2-aboutus/phone_icon.svg" alt="Phone icon" className="phone_icon"></img>
                             <p>More information</p>
                             <p>+46 8 123 122 44</p>
                     </div>
--- storeaid/src/pages/Home.jsx
+++ storeaid/src/pages/Home.jsx
@@ -4,16 +4,16 @@ import StandardButton from '../components/Standardbutton'
 export default class Home extends Component {
     render() {
         return (
-            <section id="home" class="home-section">
-                <div class="home-content">
+            <section id="home" className="home-section">
+                <div className="home-content">
                     <h3>Welcome to Storeaid</h3>
                     <h1>Space Simplified, Storage Perfected</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem fringilla sodales. Cras lobortis, libero at iaculis luctus, nisi ex pellentesque nisi, at euismod sem ipsum ac dolor.</p>
                     
-                    {/* <button class="standardbutton">Discover More</button> */}
+                    {/* <button className="standardbutton">Discover More</button> */}
                     <StandardButton text="Discover More"/>
                 </div>
-                <img src="imgs/1-home/home_man_img.svg" alt="Home section illustration" class="home-image"></img>
+                <img src="imgs/1-home/home_man_img.svg" alt="Home section illustration" className="home-image"></img>
             </section>
         )
     }
